09cc06886be0d1223a4a57b8a63444cf8ed4062b
drabinowitz <dmitrirabinowitz+code@gmail.com>
2015-02-05 15:07:00 -0500

    Update to handle offset timestamps.

--- controllers/EventsController.js
+++ controllers/EventsController.js
@@ -2,7 +2,13 @@ var Event = require('../db/event.model');
 var moment = require('moment');
 
 module.exports.getNewEvents = function (timestamp) {
-  timestamp = timestamp || moment().subtract(1, 'days').toISOString();
+  if (timestamp) {
+    console.log('preiso', timestamp);
+    timestamp = moment(timestamp).subtract(5, 'hours').toISOString();
+    console.log('postiso', timestamp);
+  } else {
+    timestamp = moment().subtract(1, 'days').toISOString();
+  }
   return new Event()
     .query('where', 'updated_at', '>', timestamp)
     .fetchAll();
