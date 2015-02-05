5a4a9f5c75a3d7cac30f1aad6c5dee539a73e1a9
drabinowitz <dmitrirabinowitz+code@gmail.com>
2015-02-05 17:45:46 -0500

    Create database first.

--- index.js
+++ index.js
@@ -1,37 +1,59 @@
-var app = require('./main');
-require('./workers/githubFetcher')();
-var debug = require('debug')('testingEB:server');
-var http = require('http');
+var mysql = require('mysql');
+var async = require('async');
 
-var port = normalizePort(process.env.PORT || '8000');
-app.set('port', port);
+mysql.createConnection({
+  host: process.env.RDS_HOSTNAME,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  port: process.env.RDS_PORT
+});
+async.series([
+  function connect(callback) {
+    client.connect(callback);
+  },
+  function clear(callback) {
+    client.query('DROP DATABASE IF EXISTS github_events', callback);
+  },
+  function create_db(callback) {
+    client.query('CREATE DATABASE github_events', callback);
+  },
+  function () {
+    var app = require('./main');
+    require('./workers/githubFetcher')();
+    var debug = require('debug')('testingEB:server');
+    var http = require('http');
 
-var server = http.createServer(app);
+    var port = normalizePort(process.env.PORT || '8000');
+    app.set('port', port);
 
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
+    var server = http.createServer(app);
 
-function normalizePort(val) {
-  var port = parseInt(val, 10);
+    server.listen(port);
+    server.on('error', onError);
+    server.on('listening', onListening);
 
-  if (isNaN(port)) {
-    return val;
+    function normalizePort(val) {
+      var port = parseInt(val, 10);
+
+      if (isNaN(port)) {
+        return val;
+      }
+
+      if (port >= 0) {
+        return port;
+      }
+
+      return false;
+    }
+
+    function onError(error) {
+      throw error;
+    }
+
+    function onListening() {
+      var addr = server.address();
+      var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+      debug('Listening on ' + bind);
+    }
   }
-
-  if (port >= 0) {
-    return port;
-  }
-
-  return false;
-}
-
-function onError(error) {
-  throw error;
-}
-
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
-  debug('Listening on ' + bind);
-}
+]);
